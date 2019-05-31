1b627fa23ea7e103ff8708fe6e6b68116c806383
Samuel T <ssamt1142@gmail.com>
2019-05-31 02:40:34 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,9 +6,9 @@ attr_accessor :name, :grade
 attr_reader :id
 
   def initialize(name, grade, id = nil)
+    @id = id
     @name = name
     @grade = grade
-    @id = id
   end
 
   def self.create_table
